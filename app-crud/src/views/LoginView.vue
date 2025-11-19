1bf5bf951fadd2fe5516a149708dc8f4c7cabc6b
Maikol-08 <mbadilloagredo@gmail.com>
2025-11-18 19:36:16 -0500

    Mockapi

--- app-crud/src/views/LoginView.vue
+++ app-crud/src/views/LoginView.vue
@@ -1,25 +1,9 @@
 <template>
   <div class="container mt-5 col-md-4">
     <h3>Iniciar Sesión</h3>
-
-    <input
-      v-model="usuario"
-      class="form-control mb-2"
-      placeholder="Usuario"
-    />
-    <input
-      v-model="clave"
-      type="password"
-      class="form-control mb-2"
-      placeholder="Contraseña"
-    />
-    <button
-      class="btn btn-primary w-100"
-      @click="login"
-    >
-      Entrar
-    </button>
-
+    <input v-model="usuario" class="form-control mb-2" placeholder="Usuario" />
+    <input v-model="clave" type="password" class="form-control mb-2" placeholder="Contraseña" />
+    <button class="btn btn-primary w-100" @click="login">Entrar</button>
     <p v-if="error" class="text-danger mt-2">{{ error }}</p>
   </div>
 </template>
@@ -27,11 +11,7 @@
 <script>
 export default {
   data() {
-    return {
-      usuario: '',
-      clave: '',
-      error: ''
-    }
+    return { usuario: '', clave: '', error: '' }
   },
   methods: {
     login() {
@@ -40,9 +20,7 @@ export default {
         { user: 'jerson', pass: '5678' }
       ]
 
-      const valido = users.find(
-        (u) => u.user === this.usuario && u.pass === this.clave
-      )
+      const valido = users.find(u => u.user === this.usuario && u.pass === this.clave)
 
       if (valido) {
         localStorage.setItem('logueado', 'true')
